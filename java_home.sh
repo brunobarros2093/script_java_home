1134ec2b2481d80261bb64bc2f39fe299da60abf
bruno barros <brunobarros2093@gmail.com>
2024-07-12 10:03:51 -0300

    added bash and comments

--- java_home.sh
+++ java_home.sh
@@ -1,3 +1,9 @@
-# Set JDK installation directory according to selected Java compiler
+#!/bin/bash
+
+# java_home.sh
+#
+# This script sets the JAVA_HOME environment variable based on the location of the currently selected
+# Java compiler (javac). It is intended to be sourced by the shell on login to ensure that JAVA_HOME
+# is always correctly configured to point to the installed Java Development Kit (JDK).
 
 export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
